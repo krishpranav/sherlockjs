3341463294daa831b39b1de4d11a6e87c1c3c2d5
krishpranav <krisna.pranav@gmail.com>
2021-06-13 09:25:05 +0530

    [+] test url function [+]

--- https-worker.js
+++ https-worker.js
@@ -1,7 +1,13 @@
 async function test(url, name) {
     return new Promise((resolve, project) => {
         require((url.protocol === 'https:') ? 'https' : 'http').get(url, res => {
-            
+            if (res.statusCode == 301 && res.headers.location) {
+                test(new URL(res.headers.location, url), name).then(bool => {
+                    resolve(bool);
+                }).catch(error => {
+                    reject(error);
+                });
+            }
         })
     })
 }
