02edd15fbf874388be0bfe4b82c35b131d154827
krishpranav <krisna.pranav@gmail.com>
2021-06-13 09:26:09 +0530

    [+] test function [+]

--- https-worker.js
+++ https-worker.js
@@ -1,17 +1,35 @@
 async function test(url, name) {
-    return new Promise((resolve, project) => {
-        require((url.protocol === 'https:') ? 'https' : 'http').get(url, res => {
-            if (res.statusCode == 301 && res.headers.location) {
-                test(new URL(res.headers.location, url), name).then(bool => {
-                    resolve(bool);
-                }).catch(error => {
-                    reject(error);
-                });
-            } else {
-                if (res.statusCode !== 200) {
-                    resolve(false);
-                }
-            }
-        })
-    })
+	return new Promise((resolve, reject) => {
+		require((url.protocol === 'https:') ? 'https' : 'http').get(url, res => {
+			if (res.statusCode === 301 && res.headers.location) {
+				test(new URL(res.headers.location, url), name).then(bool => {
+					resolve(bool);
+				}).catch(error => {
+					reject(error);
+				});
+			} else {
+				if (res.statusCode !== 200) {
+					resolve(false);
+				}
+
+				res.setEncoding('utf8');
+
+				let rawData = '';
+				res.on('data', chunk => {
+					rawData += chunk;
+				});
+
+				res.on('end', () => {
+					rawData = rawData.replace(/(<([^>]+)>)/ig, '');
+					if (rawData.includes(name) && !rawData.includes('Not Found') && !rawData.includes('Not found')) {
+						resolve(true);
+					} else {
+						resolve(false);
+					}
+				});
+			}
+		}).on('error', e => {
+			reject(e);
+		});
+	});
 }
