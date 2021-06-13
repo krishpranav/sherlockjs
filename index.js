ecc1d2a3bf43ac9c1ed2febc050e196753536d36
krishpranav <krisna.pranav@gmail.com>
2021-06-13 11:12:16 +0530

    [+] error and die [+]

--- index.js
+++ index.js
@@ -7,5 +7,9 @@
     const {fork} = require('child_process');
 
     const services = require(path.join(__dirname, 'services.json'));
-    
+
+    function errorAndDie(err) {
+        log(chalk.red.bold(`[!] Fatal error: ${err.message || err}`));
+        process.exit(1);
+    }
 })
